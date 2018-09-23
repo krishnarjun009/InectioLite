9c56f0698e37f374060bc762c0810f52a0a897f5
Krishna009 <krish.arjun009@gmail.com>
2018-09-23 10:36:14 +0530

    updated example

---
+++ Assets/Samples/ExampleProjects/BouncyBall_Inectio/Scripts/Command/InputDirectionCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Iniectio.Lite;
+using UnityEngine;
+
+namespace com.bonucyballs.Iniectio
+{
+    public class InputDirectionCommand : Command<InputDirection>
+    {
+        [Inject] private InputDirectionSignal inputDirectionSignal { get; set; }
+
+        public override void Execute(InputDirection inputDirection)
+		{
+            switch(inputDirection)
+            {
+                case InputDirection.LEFT:
+                    inputDirectionSignal.Dispatch(-1);
+                    break;
+                default:
+                    inputDirectionSignal.Dispatch(1);
+                    break;
+            }
+		}
+	}
+}
